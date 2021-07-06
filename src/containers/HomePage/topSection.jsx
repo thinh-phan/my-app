bd98191df30c981e459526a9808c1b618b794d8a
thinh-phan <phanquangthinh1997@gmail.com>
2021-07-06 22:56:35 +0700

    Added brand logo

--- src/containers/HomePage/topSection.jsx
+++ src/containers/HomePage/topSection.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { BrandLogo } from "../../components/brandLogo";
+
 import TopSectionBackgroundImg from "../../images/landing-page.jpg";
 import TheBestSpecialistsImg from "../../images/Work only with the best.png";
 
@@ -36,13 +38,39 @@ const StandoutImage = styled.div`
   }
 `;
 
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+`;
+
+const SloganText = styled.h3`
+  margin: 0;
+  line-height: 1.4;
+  color: #fff;
+  font-weight: 500;
+  font-size: 35px;
+`;
+
 export function TopSection(props) {
   return (
     <TopSectionContainer>
       <BackgroundFilter>
+        <TopSectionInnerContainer>
+          <LogoContainer>
+            <BrandLogo logoSize={60} textSize={50}/>
+
+            <SloganText>Find the right specialist</SloganText>
+            <SloganText>For the job</SloganText>
+          </LogoContainer>
+
           <StandoutImage>
-              <img src={TheBestSpecialistsImg} alt="Work only with the best.png" />
+            <img
+              src={TheBestSpecialistsImg}
+              alt="Work only with the best.png"
+            />
           </StandoutImage>
+        </TopSectionInnerContainer>
       </BackgroundFilter>
     </TopSectionContainer>
   );
